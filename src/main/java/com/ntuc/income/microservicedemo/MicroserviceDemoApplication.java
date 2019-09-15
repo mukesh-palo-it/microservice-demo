4b2dcff3a098c7920a51f8bf25fbe6dd123a809f
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-09-15 10:23:00 +0800

    Adding the first Spring boot example

---
+++ src/main/java/com/ntuc/income/microservicedemo/MicroserviceDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ntuc.income.microservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MicroserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntuc/income/microservicedemo/MicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntuc.income.microservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
